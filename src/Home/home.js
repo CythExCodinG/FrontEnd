c1de45fecb1dbc5d72253fd3f4a3db5b88a81544
EdsxCythe <edsxcythe@pop-os.localdomain>
2024-01-28 18:10:54 +0530

    navbar progress

---
+++ src/Home/home.js
@@ -0,0 +1,14 @@
+
+document.addEventListener("DOMContentLoaded",function(){
+  const men = document.getElementById('men');
+  men.style.backgroundColor="#fff";
+  men.style.color="black";
+})
+
+var gender= document.querySelector('.gender');
+
+gender.addEventListener("click",function(){
+  console.log(gender);
+})
+
+
