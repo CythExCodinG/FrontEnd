157d816f28f3fc65ee43b55c44ec62805f7c4f68
EdsxCythe <edsxcythe@pop-os.localdomain>
2024-02-21 22:08:07 +0530

    Men Products entered 2

--- src/config/data.js
+++ src/config/data.js
@@ -1,13 +1,14 @@
 const menData = [
   {
     id: 'm1',
-    title: 'OverDyed Cargo Brown',
-    category: 'Men Cargo Pants',
+    title: 'Naruto: Ghost of Uchiha',
+    category: 'Oversized T-Shirts',
     price: 799,
     imageUrl: '/public/assets/product/m1.webp',
     productDetails: {
       material:
-        `100% Cotton
+        `Premium Heavy Gauge Fabric
+        100% Cotton 
         Machine Wash`,
       origin: 'India (and proud)',
     }
